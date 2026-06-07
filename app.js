b05ee1b86df65becec84deaa6aa83a8afa5194ce
seraph <quietsolitvde@gmail.com>
2026-06-07 09:07:26 +0700

    Sorry I forgot to commit

---
+++ app.js
@@ -0,0 +1,119 @@
+// 1. Create a function that makes board with arbitrary size, but neither of the 
+// sides exceed 50 squares
+
+
+function createBoard(rows, columns) {
+    if (rows > 50 || columns > 50) return 'Error';
+    let board = [];
+    for (let i = 0 ; i < rows ; i++) {
+        board.push([]);
+        for (let j = 0 ; j < columns ; j++) {
+            board[i].push('');
+        }
+    }
+    return board;
+}
+
+// 2. Create a factory function that provides ways to interact with the board.
+// Keep the board hidden from other places in the code !
+// Try to not create a long chain of functions which rely on each other !
+
+function createBoardController(board) {
+    function showBoard() {
+        const triggerEntityStringEvolution = (boardRow) => {
+            return boardRow
+                .map(boardCell => !boardCell ? ' ' : boardCell)
+                .join('|');
+        }
+
+        let boardString = board
+            .map(row => triggerEntityStringEvolution(row))
+            .join('\n');
+
+        console.log(boardString);
+    }
+
+
+    let currentPlayer = 'X'; //Note for my terrible vertical eye scanning system: this variable is used below
+    function markCell(rowIndex, colIndex, winConditionNumber = 3) {
+        if (board[rowIndex][colIndex] !== '') {
+            console.error('Warning: You have interacted with a forbidden board cell... the system will remember this...');
+            return false;
+        };
+
+        board[rowIndex][colIndex] = currentPlayer;
+        currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+
+        checkWinStatus(rowIndex, colIndex, winConditionNumber);
+    }
+
+    // How do you check every possible winning position based on the player's last move?
+
+    function checkWinStatus(rowIndex, colIndex, winConditionNumber = 3) {
+        if (board[rowIndex] === undefined ||
+            board[rowIndex][colIndex] === undefined
+        ) {
+            console.error('The user\'s last move does not exist on the board.');
+            return false;
+        }
+
+        (function checkDirection() {
+            const algorithms = [
+                [0, 1],  // left -> right
+                [1, 0],  // bottom -> top
+                [-1, 1], // bottom left -> top right
+                [-1, -1], //bottom right -> top left
+            ];
+
+            const checkPositionValidity = (positionArray) => {
+                return board[positionArray[0]] !== undefined &&
+                        board[positionArray[0]][positionArray[1]] !== undefined
+            }
+
+            // For each direction check, follow this pattern:
+            // start at last move -> walk repeatedly -> count matches -> stop when broken
+
+            for (const direction of algorithms) {
+                let sequenceCount = 1;
+                const lastMove = board[rowIndex][colIndex]; //outputs X or O
+                let forwardPosition = [rowIndex + direction[0], colIndex + direction[1]];
+                let backwardPosition = [rowIndex - direction[0], colIndex - direction[1]];
+
+                while (
+                    checkPositionValidity(forwardPosition) &&
+                    board[forwardPosition[0]][forwardPosition[1]] === lastMove && 
+                    sequenceCount < winConditionNumber
+                ) {
+                    ++sequenceCount;
+                    forwardPosition[0] += direction[0];
+                    forwardPosition[1] += direction[1];
+                }
+
+                while (
+                    checkPositionValidity(backwardPosition) &&
+                    board[backwardPosition[0]][backwardPosition[1]] === lastMove &&
+                    sequenceCount < winConditionNumber
+                ) {
+                    ++sequenceCount;
+                    backwardPosition[0] -= direction[0];
+                    backwardPosition[1] -= direction[1];
+                }
+
+                if (sequenceCount >= winConditionNumber) {
+                    console.log("you wonnn")
+                }
+            }
+        })()
+    }
+
+    return {showBoard, markCell};
+}
+
+
+let interactions = createBoardController(createBoard(10, 10));
+interactions.markCell(0, 0);
+interactions.markCell(0, 1);
+interactions.markCell(1, 1);
+interactions.markCell(0, 2);
+interactions.markCell(2, 2);
+interactions.showBoard();
